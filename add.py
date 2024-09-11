c0f81215451bc88cc3fa490f8a882c61635561d4
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-09-11 14:58:45 -0700

    Initial commit

---
+++ add.py
@@ -0,0 +1,8 @@
+#variable a for user to input a number
+a = int(input("Input a number: "))
+#varliable b for user to input a number
+b = int(input("Input another number: "))
+#variable c to represent the sum of a and b
+c = a + b
+#printing to the console for the math above
+print("The sum of", a, "and", b, "equals:", c)
